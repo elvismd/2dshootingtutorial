31ced88de00a91a66c976043e3e973e3db543d13
Elvis L <elvis.leite.9@gmail.com>
2020-06-01 01:23:44 -0300

    second commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 10;
+
+    public void GetDamage()
+    {
+        health--;
+        Debug.Log("Enemy took damage!");
+    }
+}
