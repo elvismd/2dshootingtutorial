e9e91a63f0e5e59d6c54a4e782cbc726be8d7c82
Elvis L <elvis.leite.9@gmail.com>
2020-06-01 01:23:15 -0300

    first commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 15.0f;
+
+    void Update()
+    {
+        transform.position += transform.right * Time.deltaTime * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Bullet hit enemy!");
+
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.GetDamage();
+        }
+
+        gameObject.SetActive(false);
+        Destroy(gameObject, 2.0f);
+    }
+}
+
